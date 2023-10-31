f9bc1a911a7720ab090ad95a998d4b1d05072bb5
gaetano <gaetanosj9@gmail.com>
2023-10-31 10:07:16 +0100

    primo commit

---
+++ src/main/resources/static/js/validate.js
@@ -0,0 +1,85 @@
+function verificaRistorante(){
+    var nomeRistTxt = document.getElementById("nome_rist");
+    var descRistTxt = document.getElementById("desc_rist");
+    var ubRistElement = document.getElementById("ub_ristorante");
+
+    var nomeRist = nomeRistTxt.value.trim();
+    var descRist = descRistTxt.value.trim();
+    var ubRist = ubRistElement.value.trim();
+
+    var ok = validateRistorante(nomeRist, descRist, ubRist);
+    if (!ok){
+        alert("Mancano dei campi");
+    }else{
+        alert("Campi validati");
+    }
+}
+
+function aggiungiRistorante(){
+    var nomeRistTxt = document.getElementById("nome_rist");
+    var descRistTxt = document.getElementById("desc_rist");
+    var ubRistElement = document.getElementById("ub_ristorante");
+
+    var nomeRist = nomeRistTxt.value.trim();
+    var descRist = descRistTxt.value.trim();
+    var ubRist = ubRistElement.value.trim();
+
+    if (validateRistorante(nomeRist, descRist, ubRist)){
+        let newTr = document.createElement("tr");
+
+        let newTdChk = document.createElement("td")
+        let newTdId = document.createElement("td");
+        let newTdNome = document.createElement("td");
+        let newTdDescrizione = document.createElement("td");
+        let newTdUbicazione = document.createElement("td");
+
+        let contentNome = document.createTextNode(nomeRist);
+        let contentDescrizione = document.createTextNode(descRist);
+        let contentUbicazione = document.createTextNode(ubRist);
+
+        newTr.appendChild(newTdChk);
+        newTr.appendChild(newTdId);
+        newTr.appendChild(newTdNome);
+        newTr.appendChild(newTdDescrizione);
+        newTr.appendChild(newTdUbicazione);
+
+        newTdNome.appendChild(contentNome);
+        newTdDescrizione.appendChild(contentDescrizione);
+        newTdUbicazione.appendChild(contentUbicazione);
+
+        newTr.style = "font-weight: bold";
+
+        let tableElement = document.getElementsByTagName("table")[0];
+        tableElement.appendChild(newTr);
+    }else{
+        alert("Mancano dei campi");
+    }
+}
+
+function rimuoviRistorante(){
+    var ristorantiSelezionati = document.querySelectorAll(".ristoranteSelezionato:checked");
+    ristorantiSelezionati.forEach(function(elementSelected){
+        var valueSel = elementSelected.getAttribute("value");
+        alert(valueSel);
+    })
+}
+
+function validateRistorante(nome, descrizione, ubicazione){
+    var validationOK = true;
+    if (nome == ""){
+        validationOK = false;
+    }
+    if (descrizione == ""){
+        validationOK = false;
+    }else{
+        if (descrizione.length < 10){
+            alert("Il testo deve essere almeno 10 caratteri");
+            validationOK = false;
+        }
+    }
+
+    if (ubicazione === "---"){
+        validationOK = false;
+    }
+    return validationOK;
+}
